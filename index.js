4c6007167147de2c0007b19e6bd65b7c893bf2ff
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2022-02-07 12:11:02 -0300

    copmmit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.set('views', [path.join(__dirname, 'views/public'), path.join(__dirname, 'vi
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 
-app.listen(process.env.BASE_PORT, () => {
+app.listen(3000, () => {
 
   app.use(express.static(__dirname + '/public'))
   app.use(express.static(__dirname + '/views'))

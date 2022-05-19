aacd20fabab15eb0751cd2a5bf749cc17abd8141
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2022-05-19 12:43:11 -0300

    commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const { Client, Location, List, Buttons, LocalAuth } = require('whatsapp-web.js'
 process.title = "whatsapp-node-api"
 global.client = new Client({
   authStrategy: new LocalAuth(),
-  puppeteer: { headless: false },
+  puppeteer: { headless: false, args: ['--no-sandbox']  },
 });
 
 global.authed = false;

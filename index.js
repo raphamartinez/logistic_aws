39d68c4649cd8dd8f44c17520b971e386f399087
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2022-05-23 10:13:04 -0300

    commit

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function sleep(milliseconds) {
 process.title = "whatsapp-node-api"
 global.client = new Client({
   authStrategy: new LocalAuth(),
-  puppeteer: { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] },
+  puppeteer: { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox', '--no-cache'] },
 });
 
 global.authed = false;

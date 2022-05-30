d52f1c6378dc5c73d7f8c022af6cefddafbcba8c
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2022-05-30 10:44:36 -0300

    commit

--- api/models/driveup.js
+++ api/models/driveup.js
@@ -106,17 +106,17 @@ class DriveUp {
                     if (type.ideventtype === vehicleAlert.idEventType) {
                         const idgroup = alert.ideventtypegroup
                         if (idgroup === 9 || idgroup === 10 || idgroup === 12) {
-                            group = 'Operación Manten.'
+                            group = '120363042760809190@g.us'
                             alertType = type.description
                         }
 
                         if (idgroup === 3 || idgroup === 4 || idgroup === 5 || idgroup === 11) {
-                            group = 'Operación Disponibilidad'
+                            group = '120363024113373482@g.us'
                             alertType = type.description
                         }
 
                         if (idgroup === 2) {
-                            group = 'Operación Notific.'
+                            group = '120363024386228914@g.us'
                             switch (type.ideventtype) {
                                 case 3:
                                     alertType = `Llegada al ${vehicleAlert.customer}`
@@ -149,7 +149,7 @@ class DriveUp {
             }
             client.getChats().then((data) => {
                 data.forEach(chat => {
-                    if (chat.id.server === "g.us" && chat.name === group) {
+                    if (chat.id.server === "g.us" && chat.id == group) {
                         client.sendMessage(chat.id._serialized, message).then((response) => {
                             if (response.id.fromMe) {
                                 vehicleAlert.successend = 1
